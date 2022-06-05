829f7bde57dd5d133d158fdd10a787417172bed0
yinghaoting <yinghaoting2009@outlook.com>
2022-06-05 13:28:28 +0800

    优化随机背景图片的添加
    只需修改img_size并添加图片即可添加新背景

--- docs/NewSite/js/NormalStyle/RandomBKImg.js
+++ docs/NewSite/js/NormalStyle/RandomBKImg.js
@@ -1,8 +1,8 @@
+var img_size=5;
 var bkimg_arr=[];
-var base_src="../img/NormalStyle/RandomBKImg/";
-bkimg_arr[0]=base_src+"1.jpg";
-bkimg_arr[1]=base_src+"2.jpg";
-bkimg_arr[2]=base_src+"3.gif";
-bkimg_arr[3]=base_src+"4.jpg";
-var bkimg_num=Math.round(Math.random()*3);
+var base_src="../img/NormalStyle/RandomBKImg/GIFImg/";
+for(var i=1;i<=img_size;i++){
+	bkimg_arr[i-1]=base_src+i.toString()+".gif";
+}
+var bkimg_num=Math.round(Math.random()*(img_size-1));
 document.body.style.backgroundImage="url('"+bkimg_arr[bkimg_num]+"')";
